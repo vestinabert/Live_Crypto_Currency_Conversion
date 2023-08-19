758173e3ce9d76aa623d82358938a5e422836f12
Vestina <vestina.bertasiute@gmail.com>
2023-08-19 21:19:44 +0300

    Printing available coins lit for the user

--- crypto_converter.rb
+++ crypto_converter.rb
@@ -1,7 +1,7 @@
 require './manager'
 
 def menu
-
+    manager = Manager.new
     loop do
         puts "Cryptocurrency Converter ".center(50, "#")
         puts "a) Convert to USD"
@@ -9,6 +9,13 @@ def menu
         puts "q) Quit"
         print "Action: "
         choice = gets.chomp
+
+        case choice
+        when 'a'
+            coin_list = manager.coin_list
+            puts "Available Coins"
+            puts coin_list.join(", ").center(50, "-")
+        end
 
         break unless choice != 'q'
     end
