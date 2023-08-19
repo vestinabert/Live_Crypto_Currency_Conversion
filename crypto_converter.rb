20520f8f968655365aeea7131e0890c35fd7e24d
Vestina <vestina.bertasiute@gmail.com>
2023-08-19 20:33:46 +0300

    Adding menu instructions

---
+++ crypto_converter.rb
@@ -0,0 +1,11 @@
+def menu
+
+    loop do
+        puts "Cryptocurrency Converter ".center(50, "#")
+        puts "a) Convert to USD"
+        puts "b) Convert to EUR"
+        puts "q) Quit"
+        print "Action: "
+        choice = gets.chomp
+    end
+end
