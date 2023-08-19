589a7dc15600650d8303b25da3e32ea8c7e06ddf
Vestina <vestina.bertasiute@gmail.com>
2023-08-19 21:04:13 +0300

    Creating web scraping operation

--- manager.rb
+++ manager.rb
@@ -1,3 +1,6 @@
+require "uri"
+require "net/http"
+
 class Manager
     @@repo = {}
     def initialize
@@ -5,6 +8,12 @@ class Manager
     end
 
     def initialize_repo
+        response = web_scraper
+    end
 
+    def web_scraper
+        url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,DASH,LTC&tsyms=USD,EUR"
+        uri = URI(url)
+        Net::HTTP.get(uri)
     end
 end
