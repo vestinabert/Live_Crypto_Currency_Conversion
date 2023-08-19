70991fe46469eed37654b7caa4f58f550b9b37f3
Vestina <vestina.bertasiute@gmail.com>
2023-08-19 21:56:17 +0300

    Creating function to print calculations for both currencies

--- crypto_converter.rb
+++ crypto_converter.rb
@@ -1,5 +1,24 @@
 require './manager'
 
+def submenu(manager, to)
+    coin_list = manager.coin_list
+    puts "Available Coins"
+    puts coin_list.join(", ").center(50, "-")
+    print "Coin: "
+    symbol = gets.chomp.upcase
+    print "Amount: "
+    amount = gets.chomp.to_i
+
+        if coin_list.include? symbol
+            result = manager.calculate(amount, symbol, to)
+            puts "-".center(50, "-")
+            puts " #{amount} #{symbol} = #{result} #{to} ".center(50, "-")
+            puts "-".center(50, "-")
+        else
+            puts "#{symbol} is not available.".center(50, "*")
+        end
+end
+
 def menu
     manager = Manager.new
     loop do
@@ -11,23 +30,8 @@ def menu
         choice = gets.chomp
 
         case choice
-        when 'a'
-            coin_list = manager.coin_list
-            puts "Available Coins"
-            puts coin_list.join(", ").center(50, "-")
-            print "Coin: "
-            symbol = gets.chomp.upcase
-            print "Amount: "
-            amount = gets.chomp.to_i
-
-            if coin_list.include? symbol
-                result = manager.calculate(amount, symbol, "USD")
-                puts "-".center(50, "-")
-                puts " #{amount} #{symbol} = #{result} USD ".center(50, "-")
-                puts "-".center(50, "-")
-            else
-                puts "#{symbol} is not available.".center(50, "*")
-            end
+        when 'a' then submenu(manager, "USD")
+        when 'b' then submenu(manager, "EUR")
         end
 
         break unless choice != 'q'
